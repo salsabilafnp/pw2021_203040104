b2ee42f7cb91a44f34a707a7c64bfc93386ded8a
salsabilafnp <salsabilafebriantynp@gmail.com>
2021-03-16 16:43:35 +0700

    pertemuan 3 - kuliah

---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,26 @@
+<?php 
+// Salsabila Febrianty NP
+// 203040104
+// Control Flow
+
+// Pengkondisian/Percabangan
+// if (kondisi) else (salah)
+    //$x = 20;
+    //if($x < 20) {
+    //    echo "benar";
+    //} else {
+    //    echo "salah";
+    //}
+// if (kondisi1) else if (kondisi2) else
+    $x = 20;
+    if($x < 20) {
+        echo "benar";
+    } else if ($x == 20) {
+        echo "bingo!";
+    } else {
+        echo "salah";
+    }
+// ternary (on javascript playlist)
+// switch (on javascript playlist)
+
+?>
