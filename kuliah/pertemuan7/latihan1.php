cffcf968164b6ab8c03e09f9c32af3485f9ca492
salsabilafnp <salsabilafebriantynp@gmail.com>
2021-03-18 20:02:07 +0700

    update

---
+++ kuliah/pertemuan7/latihan1.php
@@ -0,0 +1,17 @@
+<?php 
+// Salsabila Febrianty NP
+// 203040104
+// GET & POST
+
+$_GET["Nama"] = "Salsabila Febrianty";
+$_GET["Prodi"] = "Teknik Informatika";
+
+print_r($_GET);
+echo "<br>";
+var_dump($_GET);
+
+// Minimal versi 7
+    //echo "<br>";
+    //dump($_GET);
+
+?>
