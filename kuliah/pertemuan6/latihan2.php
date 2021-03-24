b914ed87bc0d34e24e0fb64127f82a6e7edacf18
salsabilafnp <salsabilafebriantynp@gmail.com>
2021-03-24 18:19:59 +0700

    update pertemuan 6

--- kuliah/pertemuan6/latihan2.php
+++ kuliah/pertemuan6/latihan2.php
@@ -5,23 +5,37 @@
 // key-nya string yang dibuat sendiri
 
 $mahasiswa = [
-    ["nama" => "Salsabila FNP",
-    "NPM" => "203040104", 
-    "prodi" => "Teknik Informatika",
-    "email" => "salsabilafebriantynp@gmail.com"],
-    ["nama" => "Fadila C",
-    "NPM" => "182010025",
-    "prodi" => "Pendidikan Bahasa Sunda",
-    "email" => "fadilachoi@gmail.com"],
-    ["nama" => "Neng Santi M",
-    "NPM" => "181030013",
-    "prodi" => "Keperawatan",
-    "email" => "nengsantim@gmail.com"],
-    ["nama" => "Annisa RT",
-    "NPM" => "184040115",
-    "prodi" => "Ekonomi Pembangunan",
-    "email" => "regitatriar@gmail.com"]
+    [
+        "img" => "salsabila.jpg",
+        "nama" => "Salsabila FNP",
+        "npm" => "203040104", 
+        "prodi" => "Teknik Informatika",
+        "email" => "salsabilafebriantynp@gmail.com"
+    ],
+    [
+        "img" => "fadila.jpg",
+        "nama" => "Fadila C",
+        "npm" => "182010025",
+        "prodi" => "Pendidikan Bahasa Sunda",
+        "email" => "fadilachoi@gmail.com"],
+    [
+        "img" => "santi.jpg",
+        "nama" => "Neng Santi M",
+        "npm" => "181030013",
+        "prodi" => "Keperawatan",
+        "email" => "nengsantim@gmail.com"
+    ],
+    [
+        "img" => "annisa.jpg",
+        "nama" => "Annisa RT",
+        "npm" => "184040115",
+        "prodi" => "Ekonomi Pembangunan",
+        "email" => "regitatriar@gmail.com"
+    ]
 ];
+
+// Cara menampilkan satu elemen pada array multidimensi
+    //echo $mahasiswa[1]["email"];
 
 ?>
 
@@ -36,26 +50,10 @@ $mahasiswa = [
 <body>
     <h1>Daftar Mahasiswa</h1>
 
-    <!-- tampilan foreach data 1 mahasiswa -->
-    <!--<ul>
-        <?php foreach($mahasiswa as $mhs) : ?>
-            <li><?php echo $mhs ?></li>
-        <?php endforeach; ?>
-    </ul> -->
-
-    <!-- tampilan foreach untuk data array numerik multidimensi -->
-    <?php foreach($mahasiswa as $mhs) : ?>
-        <ul>
-            <li>Nama : <?= $mhs[0]; ?></li>
-            <li>NPM : <?= $mhs[1]; ?></li>
-            <li>Jurusan : <?= $mhs[2]; ?></li>
-            <li>Email : <?= $mhs[3]; ?></li>
-        </ul>
-    <?php endforeach; ?>
-
     <!-- tampilan foreach untuk array asosiatif -->
     <?php foreach($mahasiswa as $mhs) : ?>
     <ul>
+        <img src="img/<?= $mhs["img"]; ?>" width="100px">
         <li><?= $mhs["npm"]; ?></li>
         <li><?= $mhs["nama"]; ?></li>
         <li><?= $mhs["prodi"]; ?></li>
